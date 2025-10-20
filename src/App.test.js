5837d48d292cc8117806a2449e56dbf78d0f10e9
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-10-19 19:26:14 -0500

    fix bug for daploy

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders NFT card title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Equilibrium #3429/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test("renders NFT description", () => {
+  render(<App />);
+  const descriptionElement = screen.getByText(
+    /Our Equilibrium collection promotes balance and calm/i
+  );
+  expect(descriptionElement).toBeInTheDocument();
+});
+
+test("renders ETH price", () => {
+  render(<App />);
+  const priceElement = screen.getByText(/0.041 ETH/i);
+  expect(priceElement).toBeInTheDocument();
+});
+
+test("renders creator name", () => {
+  render(<App />);
+  const creatorElement = screen.getByText(/Jules Wyvern/i);
+  expect(creatorElement).toBeInTheDocument();
 });
